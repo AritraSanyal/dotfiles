c5377e445b869d5a77bc110028e0ab036e200e2e
Aritra Sanyal <aritras2003@gmail.com>
2026-03-12 00:25:12 +0530

    dap-ui removed
    remove the debug ui while running flutter apps

--- config/.config/nvim/lua/custom/plugins/flutter.lua
+++ config/.config/nvim/lua/custom/plugins/flutter.lua
@@ -8,42 +8,31 @@ return {
 
       -- Debugging
       "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
       "theHamsta/nvim-dap-virtual-text",
 
       -- Optional integrations
       "nvim-telescope/telescope.nvim",
     },
+
     config = function()
       -------------------------------------------------------
       -- DAP SETUP (Flutter / Dart)
       -------------------------------------------------------
       local dap = require("dap")
-      local dapui = require("dapui")
-
-      dapui.setup()
       require("nvim-dap-virtual-text").setup()
-
-      -- Auto open/close debugger UI
-      dap.listeners.after.event_initialized["flutter_dapui"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["flutter_dapui"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["flutter_dapui"] = function()
-        dapui.close()
-      end
 
       -------------------------------------------------------
       -- DEBUGGER KEYMAPS
       -------------------------------------------------------
       local map = vim.keymap.set
+
       map("n", "<F5>", dap.continue, { desc = "DAP Continue" })
       map("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
       map("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
       map("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+
       map("n", "<leader>b", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+
       map("n", "<leader>B", function()
         dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
       end, { desc = "DAP Conditional Breakpoint" })
@@ -52,6 +41,7 @@ return {
       -- LSP CAPABILITIES (Blink CMP)
       -------------------------------------------------------
       local capabilities = vim.lsp.protocol.make_client_capabilities()
+
       pcall(function()
         capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
       end)
@@ -60,16 +50,17 @@ return {
       -- FLUTTER TOOLS SETUP
       -------------------------------------------------------
       require("flutter-tools").setup({
+
         ui = {
           border = "rounded",
           notification_style = "native",
         },
 
         ---------------------------------------------------
-        -- DEBUGGER (VS CODE LEVEL)
+        -- DEBUGGER
         ---------------------------------------------------
         debugger = {
-          enabled = true,
+          enabled = false,
           exception_breakpoints = { "uncaught" },
           evaluate_to_string_in_debug_views = true,
         },
@@ -78,13 +69,13 @@ return {
         -- PROJECT / SDK
         ---------------------------------------------------
         root_patterns = { ".git", "pubspec.yaml" },
-        fvm = false, -- set true if you use FVM
+        fvm = false,
 
         ---------------------------------------------------
         -- UI FEATURES
         ---------------------------------------------------
         widget_guides = {
-          enabled = false, -- experimental
+          enabled = false,
         },
 
         closing_tags = {
@@ -94,14 +85,14 @@ return {
         },
 
         dev_log = {
-          enabled = true,
+          enabled = false,
           open_cmd = "15split",
           focus_on_open = true,
           notify_errors = false,
         },
 
         dev_tools = {
-          autostart = true, -- DevTools like VS Code
+          autostart = true,
           auto_open_browser = false,
         },
 
@@ -111,10 +102,11 @@ return {
         },
 
         ---------------------------------------------------
-        -- LSP (dartls is managed internally!)
+        -- LSP (dartls managed internally)
         ---------------------------------------------------
         lsp = {
           capabilities = capabilities,
+
           settings = {
             showTodos = true,
             completeFunctionCalls = true,
@@ -137,12 +129,16 @@ return {
       -------------------------------------------------------
       map("n", "<leader>fr", "<cmd>FlutterRun<CR>", { desc = "Flutter Run" })
       map("n", "<leader>fD", "<cmd>FlutterDebug<CR>", { desc = "Flutter Debug" })
+
       map("n", "<leader>fh", "<cmd>FlutterReload<CR>", { desc = "Flutter Hot Reload" })
       map("n", "<leader>fR", "<cmd>FlutterRestart<CR>", { desc = "Flutter Hot Restart" })
+
       map("n", "<leader>fd", "<cmd>FlutterDevices<CR>", { desc = "Flutter Devices" })
       map("n", "<leader>fe", "<cmd>FlutterEmulators<CR>", { desc = "Flutter Emulators" })
+
       map("n", "<leader>fo", "<cmd>FlutterOutlineToggle<CR>", { desc = "Flutter Outline" })
       map("n", "<leader>fl", "<cmd>FlutterLogToggle<CR>", { desc = "Flutter Logs" })
+
       map("n", "<leader>fq", "<cmd>FlutterQuit<CR>", { desc = "Flutter Quit" })
       map("n", "<leader>ft", "<cmd>FlutterDevTools<CR>", { desc = "Flutter DevTools" })
     end,
